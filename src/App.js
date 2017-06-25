1242e22e507829e9889647869f10f7d190d80d91
Dokeezy <darkaosis@gmail.com>
2017-06-25 10:23:57 +0200

    Premier push 🎖

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import CodeFest             from "./components/CodeFest/CodeFest"
+import Medium            from "./components/YourComponent/medium";
+
 
 import './App.css';
 
@@ -18,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CodeFest />
+        <Medium />
       </div>
     );
   }
---
+++ src/components/YourComponent/medium.js
@@ -0,0 +1,53 @@
+import React from 'react';
+// if using webpack
+// import 'medium-draft/lib/index.css';
+
+import {
+  Editor,
+  createEditorState,
+  renderHTML,
+} from 'mddrcf';
+
+import mediumDraftExporter from 'mddrcf/lib/exporter';
+import 'medium-draft/lib/basic.css'
+
+
+export default class Medium extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorState: createEditorState(), // for empty content
+    };
+
+    const editorState = this.state.editorState;
+
+    /*
+    this.state = {
+      editorState: createEditorState(data), // with content
+    };
+    */
+
+    this.onChange = (editorState) => {
+      this.setState({ editorState });
+      const renderedHTML = mediumDraftExporter(editorState.getCurrentContent());
+      console.log(renderedHTML);
+    };
+  }
+
+  componentDidMount() {
+    this.refs.editor.focus();
+  }
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <div>
+      <Editor
+        ref="editor"
+        editorState={editorState}
+        onChange={this.onChange} />
+      </div>
+    );
+  }
+};
